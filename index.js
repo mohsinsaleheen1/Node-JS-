756b65cc6761755f1e5d567971864956e7b96b32
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-09-07 20:17:23 +0500

    sync Async

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("Hello Hy");
-console.log("Ha Bhai");
-console.log("Kaisay hun ustaad");
+// console.log("Hello Hy");
+// console.log("Ha Bhai");
+// console.log("Kaisay hun ustaad");
+let fs = require("fs");
+// let data = fs.readFileSync("info.txt","utf-8");
+// console.log(data);
+// console.log("...The Ending Statement...");
+fs.readFile("info.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("...The Ending Statement...");
